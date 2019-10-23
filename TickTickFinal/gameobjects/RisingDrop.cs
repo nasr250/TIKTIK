2e8de4967737ca6ef10baf94ce9d93f3bbea87ea
nasr250 <nasr250@users.noreply.github.com>
2019-10-23 16:35:12 +0200

    ukhkjhkj

---
+++ TickTickFinal/gameobjects/RisingDrop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.Xna.Framework.Design;
+
+class RisingDrop : WaterDrop
+{
+    public override void MoveDrop()
+    {
+        if (randomcounter == 0)
+        {
+            position.Y += 20;
+            randomcounter = random.Next(100,200);
+        }
+    }
+}
+
