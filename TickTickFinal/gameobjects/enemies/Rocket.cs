b1d85b7df74cb7c86cfe30e338ae2023dbd398c0
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-10-31 17:52:38 +0100

    rocket done

--- TickTickFinal/gameobjects/enemies/Rocket.cs
+++ TickTickFinal/gameobjects/enemies/Rocket.cs
@@ -18,6 +18,7 @@ class Rocket : AnimatedGameObject
     public override void Reset()
     {
         visible = false;
+        deadRocket = false;
         position = startPosition;
         velocity = Vector2.Zero;
         spawnTime = GameEnvironment.Random.NextDouble() * 5;
@@ -30,11 +31,7 @@ class Rocket : AnimatedGameObject
         {
             spawnTime -= gameTime.ElapsedGameTime.TotalSeconds;
             return;
-        }
-        if (!deadRocket)
-        {
-            visible = true;
-        }
+        }       
         velocity.X = 600;
         if (Mirror)
         {
@@ -59,6 +56,10 @@ class Rocket : AnimatedGameObject
         if (CollidesWith(player) && visible && player.Velocity.Y >= velocity.Y)
         {
             RocketDie();
+        } 
+        if (!deadRocket)
+        {
+            visible = true;
         }
     }
 
