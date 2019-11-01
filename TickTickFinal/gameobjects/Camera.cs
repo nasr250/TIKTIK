9522423620691bb2e64a33086e887863bba3e637
nasr250 <nasr250@users.noreply.github.com>
2019-11-01 15:33:25 +0100

    Reset CAm

--- TickTickFinal/gameobjects/Camera.cs
+++ TickTickFinal/gameobjects/Camera.cs
@@ -13,10 +13,14 @@ public class Camera : GameObject
     {
         TileField tile = GameWorld.Find("tiles") as TileField;
         Player player = GameWorld.Find("player") as Player;
-        if (player.Position.X > GameEnvironment.Screen.X / 2)
+        if (player.Position.X > GameEnvironment.Screen.X / 2 && player.Position.X < tile.Columns * tile.CellWidth - GameEnvironment.Screen.X/2)
             position.X = player.Position.X - GameEnvironment.Screen.X / 2;
         if (player.Position.Y < GameEnvironment.Screen.Y / 2)
-            position.Y = player.Position.Y - GameEnvironment.Screen.Y / 2;
+          position.Y = player.Position.Y - GameEnvironment.Screen.Y / 2;
+    }
+    public void Reset()
+    {
+        position = Vector2.Zero;
     }
 }
 
