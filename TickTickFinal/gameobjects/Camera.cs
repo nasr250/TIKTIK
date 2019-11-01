1add3635f9df81c26daca62a3778778c83ac9c2c
nasr250 <nasr250@users.noreply.github.com>
2019-11-01 16:08:16 +0100

    Camera Dead werkt

--- TickTickFinal/gameobjects/Camera.cs
+++ TickTickFinal/gameobjects/Camera.cs
@@ -13,14 +13,13 @@ public class Camera : GameObject
     {
         TileField tile = GameWorld.Find("tiles") as TileField;
         Player player = GameWorld.Find("player") as Player;
-        if (player.Position.X > GameEnvironment.Screen.X / 2 && player.Position.X < tile.Columns * tile.CellWidth - GameEnvironment.Screen.X/2)
-            position.X = player.Position.X - GameEnvironment.Screen.X / 2;
-        if (player.Position.Y < GameEnvironment.Screen.Y / 2)
-          position.Y = player.Position.Y - GameEnvironment.Screen.Y / 2;
-    }
-    public void Reset()
-    {
-        position = Vector2.Zero;
+        position = player.Position + new Vector2(-GameEnvironment.Screen.X / 2, -GameEnvironment.Screen.Y / 2);
+        if (position.X < 0)
+            position.X = 0;
+        if (position.X > tile.Columns * tile.CellWidth - GameEnvironment.Screen.X)
+            position.X = tile.Columns * tile.CellWidth - GameEnvironment.Screen.X;
+        if (position.Y > tile.Rows * tile.CellHeight - GameEnvironment.Screen.Y)
+            position.Y = tile.Rows * tile.CellHeight - GameEnvironment.Screen.Y;
     }
 }
 
