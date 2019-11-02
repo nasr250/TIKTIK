36c0d74ab28e0f9f62bafbc936ad185c0a45d34c
Ruzutsas <Ruzutsas@users.noreply.github.com>
2019-11-02 18:01:06 +0100

    hhhh

--- TickTickFinal/gameobjects/Bomb.cs
+++ TickTickFinal/gameobjects/Bomb.cs
@@ -9,37 +9,21 @@ using System.Threading.Tasks;
 
 public class Bomb : AnimatedGameObject
 {
-    protected Vector2 Bombposition;
+    public static Vector2 Bombposition;
+    public static Vector2 Bombvelocity;
+
     public Bomb(Vector2 Bombposition)
     {
         LoadAnimation("Sprites/Rocket/spr_rocket@3", "default", true, 0.2f);
         PlayAnimation("default");
         Player player = GameWorld.Find("player") as Player;
-        this.Bombposition = player.GlobalPosition;        
-    }
-
-    public override void HandleInput(InputHelper inputHelper)
-    {
-        if (inputHelper.KeyPressed(Keys.X))
-        {           
-            visible = true;
-            velocity.X = 600;
-        }
+        Bombposition = player.GlobalPosition;        
     }
 
     public override void Update(GameTime gameTime)
     {
         base.Update(gameTime);
-        if (visible)
-        {
-            velocity.X = 600;
-        }
     }
 
-    public override void Reset()
-    {
-        visible = false;
-        velocity = Vector2.Zero;
-    }
 }
 
